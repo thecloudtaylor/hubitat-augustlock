c1634562bd13a6f022bee44429559d2ae6f6af98
Taylor Brown <taylorb@taylorb-surface.localdomain>
2020-11-25 11:00:38 -0800

    Initial Commit - Skeleton code

---
+++ augustlockdriver.groovy
@@ -0,0 +1,100 @@
+/*
+Hubitat Driver For August Door Lock
+(C) 2020 - Taylor Brown
+
+11-25-2020 :  Initial 
+*/
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+metadata {
+    definition (name: "August WiFI Lock", namespace: "thecloudtaylor", author: "Taylor Brown") {
+        capability "Lock"
+        capability "Actuator"
+        capability "Refresh"
+        capability "ContactSensor"
+    }
+
+    preferences{
+        input ("debugLogs", "bool", 
+			   title: "Enable debug logging", 
+			   defaultValue: false)
+		input ("descriptionText", "bool", 
+			   title: "Enable description text logging", 
+			   defaultValue: true)
+    }
+}
+
+void LogDebug(logMessage)
+{
+    if(debugLogs)
+    {
+        log.debug "${device.displayName} ${logMessage}";
+    }
+}
+
+void LogInfo(logMessage)
+{
+    log.info "${device.displayName} ${logMessage}";
+}
+
+void LogWarn(logMessage)
+{
+    log.warn "${device.displayName} ${logMessage}";
+}
+
+void disableDebugLog() 
+{
+    LogWarn("Disabling Debug Logging.");
+    device.updateSetting("debugLogs",[value:"false",type:"bool"]);
+}
+
+void installed()
+{
+    LogInfo("Installing.");
+}
+
+void uninstalled()
+{
+    LogInfo("Uninstalling.");
+}
+
+void updated() 
+{
+    LogInfo("Updating.")
+}
+
+void parse(String message) 
+{
+    LogDebug("ParseCalled: ${message}");
+}
+
+void refresh() 
+{
+    LogDebug("RefreshCalled");
+
+}
+
+void lock()
+{
+    LogInfo("Locking Door");
+}
+
+void unlock()
+{
+    LogInfo("Unlocking Door");
+}
+
+void dooropened()
+{
+    LogDebug("DoorOpenedCalled");
+
+    sendEvent(name:"contact", value: "open", isStateChange: false, descriptionText: "Door Opened");
+}
+
+void doorclosed()
+{
+    LogDebug("DoorClosedCalled");
+
+    sendEvent(name:"contact", value: "closed", isStateChange: false, descriptionText: "Door Closed");
+}
