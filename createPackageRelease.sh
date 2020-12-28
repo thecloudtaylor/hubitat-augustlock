0d3b4aa0ede6a3c2dde4a3b57a2fc0fb3af18d67
Taylor Brown <taylor@thecloudtaylor.com>
2020-12-28 15:10:42 -0800

    New Package Version

--- createPackageRelease.sh
+++ createPackageRelease.sh
@@ -16,7 +16,7 @@ if [[ -z "${version}" ]] || [[ -z "${codePath}" ]] || [[ -z "${relNotes}" ]]; th
   echo "Not all requied parmiters passed!"; echo $usage; exit 1 ;
 fi
 
-appPath="$codePath/augusthomeapp.groovy.groovy"
+appPath="$codePath/augusthomeapp.groovy"
 lockDriverPath="$codePath/drivers/august_lock.groovy"
 lockDoorSenseDriverPath="$codePath/drivers/august_lock_doorsense.groovy"
 keyPadDriverPath="$codePath/drivers/august_keypad.groovy"
@@ -29,8 +29,8 @@ echo "lockDoorSenseDriverPath: $lockDoorSenseDriverPath"
 echo "keyPadDriverPath: $keyPadDriverPath"
 echo "ReleaseNotes: $relNotes"
 
-hpm manifest-modify-driver --id e2490bbd-ed44-4141-a270-f3c6e6b9771c --version=$version --location $lockDoorSenseDriverPath packageManifest.json
-hpm manifest-modify-driver --id 86e5b69e-8809-47a9-b106-0ef0116bf3c7 --version=$version --location $lockDriverPath packageManifest.json
-hpm manifest-modify-driver --id 030c8220-ec97-475b-9fc8-db157b6bccd0 --version=$version --location $keyPadDriverPath packageManifest.json
-hpm manifest-modify-app --id 3b8f568d-b664-4d6f-94cd-ec319319ed1b --version=$version --location $appPath packageManifest.json
-hpm manifest-modify --releasenotes="$relNotes" --version=$version packageManifest.json
+hpm manifest-modify-driver --id e2490bbd-ed44-4141-a270-f3c6e6b9771c --version=$version --location $lockDoorSenseDriverPath ../hubitat-packages/packages/augustManifest.json
+hpm manifest-modify-driver --id 86e5b69e-8809-47a9-b106-0ef0116bf3c7 --version=$version --location $lockDriverPath ../hubitat-packages/packages/augustManifest.json
+hpm manifest-modify-driver --id 030c8220-ec97-475b-9fc8-db157b6bccd0 --version=$version --location $keyPadDriverPath ../hubitat-packages/packages/augustManifest.json
+hpm manifest-modify-app --id 3b8f568d-b664-4d6f-94cd-ec319319ed1b --version=$version --location $appPath ../hubitat-packages/packages/augustManifest.json
+hpm manifest-modify --releasenotes="$relNotes" --version=$version ../hubitat-packages/packages/augustManifest.json
